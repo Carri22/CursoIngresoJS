6aa34b222a117d8e00d8591afc9bfeaa71828700
Carri22 <lucascarrizo11@gmail.com>
2022-07-20 12:28:38 -0300

    Ejercicio 9 IF

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,52 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+	let estadia;
+	let mensaje;
+
+	let destinoIngresado;
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+
+	let estacionIngresada;
+	estacionIngresada = document.getElementById("txtIdEstacion").value;
+
+	estadia = 1500;
+
+	switch (estacionIngresada) {
+		case "Invierno":
+			switch (destinoIngresado) {
+				case "Bariloche":
+				case "Mar del plata":
+					mensaje = estadia * 1.2;
+					break;
+				default:
+					mensaje = estadia * 0.9;
+					break;
+			}
+
+		case "Verano":
+			switch (destinoIngresado) {
+				case "Bariloche":
+					mensaje = estadia * 0.8;
+				case "Mar del plata":
+					mensaje = estadia * 1.2;
+					break;
+				default:
+					mensaje = estadia * 1.1;
+					break;
+			}
+		default:
+			switch (destinoIngresado) {
+				case "Cordoba":
+					mensaje = estadia;
+					break;
+				default:
+					mensaje = estadia * 1.1;
+					break;
+			}
+	}
+	alert(mensaje.toFixed(2));
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
