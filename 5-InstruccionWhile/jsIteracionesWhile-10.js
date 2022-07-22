90570f35d2b56135e7ec8f6293bc14a81b298be9
Carri22 <108766795+Carri22@users.noreply.github.com>
2022-07-22 11:49:03 -0300

    While 10

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -10,20 +10,65 @@ hasta que el usuario quiera, mostrar:
 7-Promedio de positivos.
 8-Promedios de negativos.
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
+
+
+//IMPORTANTE: Este programa no considera el numero 0 como positivo.
+
+
 function mostrar()
 {
 	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let respuesta;
+	let numeroIngresado;
+	let sumaNegativos=0;
+	let sumaPositivos=0;
+	let contPositivos=0;
+	let contNegativos=0;
+	let contCeros=0;
+	let contPares=0;
+	let promPositivos;
+	let promNegativos;
+	let diferenciaPosNeg;
 
-	respuesta="si";
+	respuesta=true;
 
-	while(respuesta=="si")
+	while(respuesta==true)
 	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		numeroIngresado=prompt("Ingrese un numero");
+		numeroIngresado=parseInt(numeroIngresado);
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+			if(numeroIngresado>0){
+				sumaPositivos+=numeroIngresado;
+				contPositivos++;
+			}
+			else if(numeroIngresado<0)
+			{
+				sumaNegativos+=numeroIngresado;
+				contNegativos++;	
+			}
+			else{
+				contCeros++;
+			}
+			
+			if(numeroIngresado%2==0){
+				contPares++;
+			}
+			
+		
+		respuesta=confirm("desea continuar?");
+	}//fin del while
+	promPositivos=sumaPositivos/contPositivos;
+	promNegativos=sumaNegativos/contNegativos;
+	diferenciaPosNeg=contPositivos-contNegativos;
+
+	document.write("La suma de negativos es :"+sumaNegativos+"<br>"+
+				"La suma de los positivos :"+sumaPositivos+"<br>"+
+				"La cantidad de positivos es :"+contPositivos+"<br>"+
+				"La cantidad de negativos es :"+contNegativos+"<br>"+
+				"La cantidad de números pares es :"+contPares+"<br>"+
+				"La cantidad de ceros es :"+contCeros+"<br>"+
+				"El promedio de positivos es :"+promPositivos.toFixed(2)+"<br>"+
+				"El promedios de negativos es :"+promNegativos.toFixed(2)+"<br>"+
+				"la diferencia entre positivos y negativosos es :"+diferenciaPosNeg);
+
 }//FIN DE LA FUNCIÓN
