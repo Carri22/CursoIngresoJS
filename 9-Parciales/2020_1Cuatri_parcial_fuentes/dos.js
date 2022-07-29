77e210b705e002388419d68bc8f488693c83fbd1
Carri22 <lucascarrizo11@gmail.com>
2022-07-29 10:51:37 -0300

    Parcial 9, uno dos y cuatro

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,104 @@
-function mostrar()
+/*Realizar el algoritmo que permita ingresar 5 paises:
+del continente (validar entre america , asia , europa ,africa y oceania)
+el nombre del país,
+cantidad de habitantes en millones entre 1 y 2000 (validar)
+la temperaruta mínima que se registra en su territorio(entre -50 y 50)
+*/ /*Se debe Informar al usuario lo siguiente:
+a)La cantidad de temperaturas pares.
+b)El nombre del pais con menos habitantes
+c)la cantidad de paises de america que tienen menos de 0 grados .
+d)el promedio de habitantes entre los paises ingresados .
+e) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura.
+*/
+
+function mostrar() 
 {
-  alert("dos");
+
+  let nombrePais;
+  let continente;
+  let temperatura;
+  let cantHabitantes;
+  let cantTempPares;
+  let paisMenosHab;
+  let tempMin;
+  let paisMenorTemp;
+  let cont;
+  let flag = true;
+  let flagDos = true;
+  let cantPaisTempCero;
+  let promedio;
+
+
+  let acuHabitantes;
+  let sPaisMenosHab;
+
+  acuHabitantes = 0;
+  cont = 0;
+  cantTempPares = 0;
+  cantPaisTempCero = 0;
+
+  while (cont < 3)
+  {
+    continente = prompt("Por favor ingrese el continente");
+    continente = continente.toLowerCase();
+    while (continente != "america" && continente != "asia" && continente != "europa" && continente != "africa" && continente != "oceania") {
+      continente = prompt("Por favor ingrese el continente");
+      continente = continente.toLowerCase();
+    }
+
+    nombrePais = prompt("Por favor ingrese el pais");
+    nombrePais = nombrePais.toLowerCase();
+
+    cantHabitantes = prompt("Por favor ingrese la cantidad de habitantes");
+    cantHabitantes = parseInt(cantHabitantes);
+    while (isNaN(cantHabitantes) || cantHabitantes < 1 || cantHabitantes > 2000) {
+      cantHabitantes = prompt("Por favor reingrese la cantidad de habitantes");
+      cantHabitantes = parseInt(cantHabitantes);
+    }
+
+    temperatura = prompt("Ingrese la temperatura minima del territorio");
+    temperatura = parseInt(temperatura);
+    while (isNaN(temperatura) || temperatura < -50 || temperatura > 50) {
+      temperatura = prompt("Error, reingrese la temperatura");
+      temperatura = parseInt(temperatura);
+    }
+
+    cont++;
+
+    if (temperatura % 2 == 0) {
+      cantTempPares++;
+    }
+
+    if (flag == true || sPaisMenosHab > cantHabitantes) {
+      flag = false;
+      sPaisMenosHab = cantHabitantes;
+      paisMenosHab = nombrePais;
+    }
+
+    if (continente == "america") {
+      if (temperatura < 0) {
+        cantPaisTempCero++;
+      }
+  
+    }
+  
+    if (flagDos == true || temperatura > tempMin) {
+      tempMin = temperatura;
+      paisMenorTemp = nombrePais;
+    }
+
+    acuHabitantes += cantHabitantes;
+    //promedio = cantHabitantes/cont; 
+  }
+
+  promedio = cantHabitantes/cont; 
+  
+  document.write("La cantidad de temperaturas pares son : " + cantTempPares + "<br>");
+  document.write("El nombredel pais con menos habitantes es: " + paisMenosHab + "<br>");
+  document.write("el promedio de habitantes entre los paises ingresados es: " + promedio + "<br>");
+  document.write("la temperatura minima ingresada es: " + tempMin + " pertence al pais: " + paisMenorTemp);
+
 }
+
+
+
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,119 @@
+/*
+En el ingreso a un viaje en crucero nos solicitan 
+nombre , 
+edad(mayores de 18), 
+sexo("f" o "m") y 
+estado civil("soltero", "casado" o "viudo");
+a) El nombre del hombre casado más joven.
+b) El sexo y nombre del pasajero/a más viejo.
+c) La cantidad de mujeres que hay casadas o viudas.
+d) El promedio de edad entre las mujeres.
+e) El promedio de edad entre los hombres solteros. 
+*/
+function mostrar() {
 
-function mostrar()
-{
-	alert("uno");
+	let nombre;
+	let edad;
+	let sexo;
+	let estado;
+	let promedioMujeres;
+	let promedioHombres;
+	
+
+	let minCasado;
+	let nombreMinCasado;
+
+	let maxEdad;
+	let maxNombreEdad;
+	let maxSexoEdad;
+
+	let respuesta = true;
+	let flagCasado = true;
+	let flagEdad=true;
+
+	let contEdadMujer=0;
+	let contMujeres=0;
+	let contMujeresEnTotal=0;
+	let contHombres=0;
+	let contEdadHombres=0;
+	
+	do {
+
+		nombre = prompt("ingrese su nombre").toLowerCase();
+		while (isNaN(nombre) == false) {
+			nombre = prompt("Error, no ingreso un nombre valido, vuelva a intentarlo").toLowerCase();
+		}
+
+		edad = prompt("ingrese su edad").toLowerCase();
+		edad = parseInt(edad);
+		while (isNaN(edad)) {
+			edad = prompt("Error, no ingreso una edad valida, vuelva a intentarlo").toLowerCase();
+			edad = parseInt(edad);
+		}
+
+		sexo = prompt("ingrese el sexo, f para femenino, m para masculino").toLowerCase();
+		while (sexo != "f" && sexo != "m") {
+			sexo = prompt("Error, no ingreso un sexo valido, vuelva a intentarlo").toLowerCase();
+		}
+
+		estado = prompt("ingrese su estado civil, soltero o soltero/casado o casada/viudo o viuda").toLowerCase();
+		while (estado != "soltero" && estado != "soltera" &&estado != "casado" && estado != "casada" && estado != "viudo" &&estado != "viuda") {
+			estado = prompt("Error, no ingreso un estado civil valido, vuelva a intentarlo").toLowerCase();
+		}
+
+		if (estado == "casado") {
+			if (flagCasado == true) {
+				flagCasado = false;
+				minCasado = edad;
+				nombreMinCasado = nombre;
+			}
+			else if (minCasado > edad) {
+				minCasado = edad;
+				nombreMinCasado = nombre;
+			}
+		}
+
+
+		if (flagEdad == true) {
+			flagEdad = false;
+			maxEdad = edad;
+			maxNombreEdad = nombre;
+			maxSexoEdad = sexo;
+		}
+		else if (maxEdad < edad) {
+			maxEdad = edad;
+			maxNombreEdad = nombre;
+			maxSexoEdad = sexo;
+		}
+
+		if(sexo=="f"){
+
+			contMujeresEnTotal++;
+			contEdadMujer+=edad;
+
+			if(estado=="casado" ||estado=="casado" || estado=="viuda"||estado=="viudo"){
+				contMujeres++;
+			}
+
+		}
+		else{
+			if(estado=="soltero"){
+				contHombres++;
+				contEdadHombres+=edad;
+			}
+		}
+
+		respuesta = confirm("desea continuar?");
+	} while (respuesta == true);
+
+	promedioMujeres = contEdadMujer/contMujeresEnTotal;
+	promedioMujeres.toFixed(2);
+	promedioHombres = contEdadHombres/contHombres;
+	promedioHombres.toFixed(2);
+
+	document.write("El nombre del hombre casado mas joven es: " + nombreMinCasado + "<br>");
+	document.write("El nombre del pasajero mas viejo es: " + maxNombreEdad + " y su sexo es " +maxSexoEdad+"<br>");
+	document.write("la cantidad de mujeres casadas y viudas son: " + contMujeres + "<br>");
+	document.write("El promedio de edad de las mujeres es:  " + promedioMujeres + "<br>");
+	document.write("El promedio de edad de los hombres solteros es:  " + promedioHombres + "<br>");
 }
